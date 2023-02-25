50784b5e9b9275c21ad69eddc15ceac57c69c324
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-24 22:11:57 -0300

    :sparkles: feat: create controller Usuario

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEnd_Simple_CRUD_in_C_SHARP_MySQL.Controllers
+{
+  [ApiController]
+  [Route("/usuario")]
+  public class UsuarioController : ControllerBase
+  {
+    [HttpGet]
+
+    public string Get()
+    {
+      return "OK";
+    }
+  }
+}
