09045172029cc07b699a6209ace3ca5e80c9810c
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-25 15:19:32 -0300

    :sparkles: feat: add UserRepository in the UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,4 @@
+using BackEnd_Simple_CRUD_in_C_SHARP_MySQL.Repository;
 using Microsoft.AspNetCore.Mvc;
 namespace BackEnd_Simple_CRUD_in_C_SHARP_MySQL.Controllers
 {
@@ -6,6 +7,13 @@ namespace BackEnd_Simple_CRUD_in_C_SHARP_MySQL.Controllers
   [Route("/users")]
   public class UserController : ControllerBase
   {
+    private readonly IUserRepository _repository;
+
+    public UserController(IUserRepository repository)
+    {
+      _repository = repository;
+    }
+
     [HttpGet]
     public string Get()
     {
