7ebfa2f4b03a9089eb88f238ab32c726b0373956
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-25 10:33:02 -0300

    :sparkles: feat: create connectionString and connection database in the life cycle

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using BackEnd_Simple_CRUD_in_C_SHARP_MySQL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<UsuarioContext>(options => {
+    string connectionString = builder.Configuration.GetConnectionString("Default");
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+});
 
 var app = builder.Build();
 
