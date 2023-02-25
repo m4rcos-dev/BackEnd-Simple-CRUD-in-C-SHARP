097df8bd1517d59e2042449bbe6a250511fab8cf
m4rcos-dev <dev.marcospereira@gmail.com>
2023-02-24 22:03:25 -0300

    :sparkles: feat: create model Usuario

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,10 @@
+namespace BackEnd_Simple_CRUD_in_C_SHARP_MySQL.Models
+{
+
+  public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
